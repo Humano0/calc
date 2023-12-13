2e61b9bbcf31ab30212c44747c9778c1cc43c510
Eyüp Can Yönter <oniraca.om00@gmail.com>
2023-12-13 17:00:04 +0300

    frontend init

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,29 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='board'></div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>AC</div>
+      <div>%</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div className='operators'>+</div>
+      <div className='operators'>-</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div className='operators'>x</div>
+      <div className='operators'>÷</div>
+      <div className='zerodiv'>0</div>
+      <div>.</div>
+      <div className='enter operators'>=</div>
+    </div>
+  );
+}
+
+export default App;
